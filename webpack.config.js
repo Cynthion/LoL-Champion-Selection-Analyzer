4828a818b41ce77c743697967c29b458741bf555
Christian Lüthold <biocodr@gmail.com>
2017-03-18 01:38:51 +0100

    build and watch scripts fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,78 @@
+const webpack = require('webpack');
 const path = require('path');
+const helpers = require('./helpers');
+
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config = {
+    devtool: 'source-map',
+
     entry: {
-        // bundle all Angular2 scripts and serve them from a single file
-        'angular2': [
-            'rxjs',
-            'reflect-metadata',
-            'angular2/core',
-            'angular2/router',
-            'angular2/http'
-        ],
-        'app': './app/app'
+        'polyfills': './src/polyfills.ts',
+        'vendor': './src/vendor.ts',
+        'app': './src/app/app',
     },
     output: {
-        path: __dirname + '/build',
-        publicPath: '/build',
-        filename: '[name].bundle.js',
-        sourceMapFilename: '[file].js.map',
+        path: helpers.root('src/app/dist'),
+        filename: '[name].js',
+        sourceMapFilename: '[file].map',
         chunkFilename: '[id].chunk.js'
     },
     resolve: {
-        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.css', '.html']
+        // Array of extensions that should be used to resolve modules
+        extensions: ['.ts', '.tsx', '.js', '.json', '.css', '.html'],
+
+        // Array of directory names to be resolved to the current directory
+        modules: [helpers.root('src'), 'node_modules']
     },
     module: {
         rules: [
+            // Support for .ts and .tsx files
             { 
                 test: /\.ts(x?)$/, 
-                exclude: /node_modules/,
-                use: 'ts-loader' 
+                exclude: [/\.(spec|e2e)\.ts$/],
+                use: ['awesome-typescript-loader', 'angular2-template-loader']
+            },
+            // Support for .json files
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            // Support for .html and .css as raw text
+            {
+                test: /\.html$/,
+                loader: 'raw-loader',
+                exclude: [helpers.root('app/index.html')]
             }
         ]
     },
     plugins: [
-
+        // Plugin: CommonsChunkPlugin
+        // Description: Shares common code between the pages.
+        // It identifies common modules and put them into a commons chunk.
+        //
+        // See: https://webpack.github.io/docs/list-of-plugins.html#commonschunkplugin
+        // See: https://github.com/webpack/docs/wiki/optimization#multi-page-app
+        new webpack.optimize.CommonsChunkPlugin({ name: ['vendor', 'polyfills'], minChunks: Infinity }),
+        // Plugin: CopyWebpackPlugin
+        // Description: Copy files and directories in webpack.
+        //
+        // Copies project static assets.
+        //
+        // See: https://www.npmjs.com/package/copy-webpack-plugin
+        new CopyWebpackPlugin([{ from: 'src/assets', to: 'assets' }]),
     ]
+    // we need this due to problems with es6-shim
+    // node: {
+    //     global: true,
+    //     progress: false,
+    //     crypto: 'empty',
+    //     module: false,
+    //     clearImmediate: false,
+    //     setImmediate: false
+    // }
 };
 
+// TODO used?
+// config.target = 'electron-renderer';
 module.exports = config;
