312c68c3000c446cd20ecd8d94d9c78ff38ee4d6
Christian Lüthold <biocodr@gmail.com>
2017-03-20 00:24:20 +0100

    SummonerService added

---
+++ WebApi/WebApi.IntegrationTests/Services/SummonerServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApi.Services;
+using WebApi.Services.Interfaces;
+
+namespace WebApi.IntegrationTests.Services
+{
+    [TestClass]
+    public class SummonerServiceTests
+    {
+        [TestMethod]
+        public void GivenSummonerName_WhenGetSummonersByName_ThenResponseIsSuccessful()
+        {
+            // arrange
+            const string summonerName = "Drohkan";
+
+            // act
+            var pairs = GetSummonerService().GetSummonersByNameAsync(new[] {summonerName}).Result.ToArray();
+            Console.WriteLine($"{ nameof(SummonerServiceTests) } retrieved: { string.Join(", ", pairs.Select(d => d.Value.ToString())) }");
+
+            // assert
+            Assert.AreEqual(1, pairs.Length);
+            Assert.AreEqual(summonerName, pairs.First().Value.Name);
+        }
+
+        private static ISummonerService GetSummonerService()
+        {
+            // TODO use DI
+            var apiKey = RiotApiKey.CreateFromFile();
+
+            var webService = new WebService(apiKey);
+            var regionSelector = new RegionSelector();
+
+            return new SummonerService(webService, regionSelector);
+        }
+    }
+}
