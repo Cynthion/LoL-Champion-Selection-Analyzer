cce588ad2f2a2ea5b73f4e381bc2ba27537b6c56
Christian Lüthold <christian.luethold@zuehlke.com>
2017-03-20 22:27:10 +0100

    added data model

---
+++ WebApi/WebApi/Models/Stats/ChampionStats.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi.Models.Stats
+{
+    public class ChampionStats
+    {
+        /// <summary>
+        /// Champion ID. Note that champion ID 0 represents the combined stats for all champions.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Aggregated stats associated with the champion.
+        /// </summary>
+        public AggregatedStats Stats { get; set; }
+    }
+}
