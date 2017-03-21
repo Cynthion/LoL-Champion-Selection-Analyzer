f2a87d76033c2a2cb08f9a7ada3e01ea767ce229
Christian Lüthold <biocodr@gmail.com>
2017-03-21 01:05:44 +0100

    in memory database added for unit testing

---
+++ WebApi/WebApi.UnitTests/DatabaseHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.UnitTests
+{
+    public class DatabaseHelper
+    {
+        public static DbContextOptions GetInMemoryContext<TContext>([CallerMemberName] string callingFunction = null) where TContext : DbContext
+        {
+            return new DbContextOptionsBuilder<TContext>()
+                .UseInMemoryDatabase(callingFunction ?? "TestDatabaseName")
+                .Options;
+        }
+    }
+}
