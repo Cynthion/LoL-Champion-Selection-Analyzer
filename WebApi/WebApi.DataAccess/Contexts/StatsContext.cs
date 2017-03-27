705329f10b3fc6d15a5e2c34415653154c3d5ffe
Christian Lüthold <biocodr@gmail.com>
2017-03-27 23:03:07 +0200

    added DataAccess project

---
+++ WebApi/WebApi.DataAccess/Contexts/StatsContext.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApi.DataAccess.Contexts
+{
+    //public class StatsContext : DbContext
+    //{
+    //    public StatsContext(DbContextOptions<StatsContext> options)
+    //        : base(options)
+    //    {
+    //    }
+
+    //    public DbSet<AggregatedInfo> AggregatedInfos { get; set; }
+
+    //    public DbSet<ChampionInfo> ChampionInfos { get; set; }
+
+    //    public DbSet<RankedInfo> RankedInfos { get; set; }
+
+    //    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //    {
+    //        modelBuilder.Entity<AggregatedInfo>().ToTable("AggregatedInfo");
+    //        modelBuilder.Entity<ChampionInfo>().ToTable("ChampionInfo");
+    //        modelBuilder.Entity<RankedInfo>().ToTable("RankedInfo");
+    //    }
+    //}
+}
