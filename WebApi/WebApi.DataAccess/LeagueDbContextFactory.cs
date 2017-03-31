6dfde2c0b9c27bace38db7eca0eaa843c23ea547
Christian Lüthold <biocodr@gmail.com>
2017-03-31 19:53:01 +0200

    add hacky workaround for EF Core being able to resolve DI in separate DLL

---
+++ WebApi/WebApi.DataAccess/LeagueDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using WebApi.DataAccess.DbContexts;
+
+namespace WebApi.DataAccess
+{
+    public class LeagueDbContextFactory : IDbContextFactory<LeagueContext>
+    {
+        public LeagueContext Create(DbContextFactoryOptions options)
+        {
+            var builder = new DbContextOptionsBuilder<LeagueContext>();
+            builder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=WebApi;Trusted_Connection=True;MultipleActiveResultSets=true");
+            return new LeagueContext(builder.Options);
+        }
+    }
+}
---
+++ WebApi/WebApi.DataAccess/Migrations/20170331174202_CreateLeagueContext.Designer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using WebApi.DataAccess.DbContexts;
+
+namespace WebApi.DataAccess.Migrations
+{
+    [DbContext(typeof(LeagueContext))]
+    [Migration("20170331174202_CreateLeagueContext")]
+    partial class CreateLeagueContext
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("WebApi.Model.Dtos.League.LeagueDto", b =>
+                {
+                    b.Property<string>("ParticipantId");
+
+                    b.Property<string>("Queue");
+
+                    b.Property<string>("Tier");
+
+                    b.HasKey("ParticipantId");
+
+                    b.ToTable("League");
+                });
+
+            modelBuilder.Entity("WebApi.Model.Dtos.League.LeagueEntryDto", b =>
+                {
+                    b.Property<string>("PlayerOrTeamId");
+
+                    b.Property<string>("Division");
+
+                    b.Property<string>("LeagueDtoParticipantId");
+
+                    b.Property<string>("PlayerOrTeamName");
+
+                    b.HasKey("PlayerOrTeamId");
+
+                    b.HasIndex("LeagueDtoParticipantId");
+
+                    b.ToTable("LeagueEntry");
+                });
+
+            modelBuilder.Entity("WebApi.Model.Dtos.League.LeagueEntryDto", b =>
+                {
+                    b.HasOne("WebApi.Model.Dtos.League.LeagueDto")
+                        .WithMany("Entries")
+                        .HasForeignKey("LeagueDtoParticipantId");
+                });
+        }
+    }
+}
