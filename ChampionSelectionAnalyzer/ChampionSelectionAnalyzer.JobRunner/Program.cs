40cd0a055475e48ef5d4750fa22b410e4106c241
Christian Luethold <christian.luethold@zuehlke.com>
2018-11-01 17:17:58 +0100

    setup jobrunner program

--- ChampionSelectionAnalyzer/ChampionSelectionAnalyzer.JobRunner/Program.cs
+++ ChampionSelectionAnalyzer/ChampionSelectionAnalyzer.JobRunner/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Threading;
+using ChampionSelectionAnalyzer.RiotApiClient.Misc;
+using ChampionSelectionAnalyzer.RiotApiClient.Misc.Interfaces;
+using ChampionSelectionAnalyzer.RiotApiClient.Services;
+using ChampionSelectionAnalyzer.RiotApiClient.Services.Interfaces;
+using Lamar;
+using NLog;
 
 namespace ChampionSelectionAnalyzer.JobRunner
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cts = new CancellationTokenSource();
+
+            try
+            {
+                SetupIoC();
+
+                new RiotPollingService().ExecutePolling(cts.Token);
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Logger.Log(LogLevel.Error, e.Message, "Unexpected termination due to an error;");
+
+                Console.ReadLine();
+            }
+            finally
+            {
+                cts.Cancel();
+            }
+        }
+
+        private static void SetupIoC()
+        {
+            var container = new Container(c =>
+            {
+                c.For<IApiKey>().Use(a =>
+                {
+                    var apiKey = RiotApiKey.CreateFromFile();
+                    Logger.Info($"Using API Key:\n{apiKey}");
+                    return apiKey;
+                });
+
+                c.For<IWebService>().Use(RiotWebService.Instance);
+
+                c.For<ILeagueService>().Use<LeagueService>();
+            });
         }
     }
 }
---
+++ ChampionSelectionAnalyzer/ChampionSelectionAnalyzer.JobRunner/RiotPollingService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using ChampionSelectionAnalyzer.JobRunner.Framework;
+
+namespace ChampionSelectionAnalyzer.JobRunner
+{
+    internal class RiotPollingService
+    {
+        private readonly IEnumerable<IJobRunner> _jobRunners = new List<IJobRunner>();
+
+        public void ExecutePolling(CancellationToken cancellationToken)
+        {
+            cancellationToken.Register(OnStopped);
+        }
+
+        private void OnStopped()
+        {
+            foreach (var jobRunner in _jobRunners)
+            {
+                jobRunner.Stop();
+            }
+        }
+    }
+}
